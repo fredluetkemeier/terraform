d373f6698233e4f4df774f76554ca0a6e999ab6d
Fred Luetkemeier <fredluetkemeier@gmail.com>
2022-04-18 15:10:47 -0500

    Add aws provider

--- single-server/main.tf
+++ single-server/main.tf
@@ -1,3 +1,8 @@
+provider "aws" {
+  region  = "us-east-2"
+  profile = "fred.luetkemeier"
+}
+
 resource "aws_instance" "example" {
   ami           = "ami-02e67f70dd7967c1f"
   instance_type = "t2.micro"
