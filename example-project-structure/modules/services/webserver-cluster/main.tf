9b5945918647845d4d406b49f005b6374d3f7907
Fred Luetkemeier <fredluetkemeier@gmail.com>
2022-04-27 15:39:53 -0500

    Prepare for deployment

--- example-project-structure/modules/services/webserver-cluster/main.tf
+++ example-project-structure/modules/services/webserver-cluster/main.tf
@@ -27,7 +27,7 @@ data "terraform_remote_state" "db" {
 }
 
 data "template_file" "user_data" {
-  template = file("user-data.sh")
+  template = file("${path.module}/user-data.sh")
 
   vars = {
     server_port = var.server_port
@@ -130,29 +130,39 @@ resource "aws_lb_target_group" "asg" {
 
 resource "aws_security_group" "instance" {
   name = "${var.cluster_name}-instance"
-
-  ingress {
-    from_port   = var.server_port
-    to_port     = var.server_port
-    protocol    = local.tcp_protocol
-    cidr_blocks = local.all_ips
-  }
 }
+
+resource "aws_security_group" "allow_http_inbound" {
+  type              = "ingress"
+  security_group_id = aws_security_group.instance.id
+
+  from_port   = var.server_port
+  to_port     = var.server_port
+  protocol    = local.tcp_protocol
+  cidr_blocks = local.all_ips
+}
+
 
 resource "aws_security_group" "alb" {
   name = "${var.cluster_name}-alb"
+}
 
-  ingress {
-    from_port   = local.http_port
-    to_port     = local.http_port
-    protocol    = local.tcp_protocol
-    cidr_blocks = local.all_ips
-  }
+resource "aws_security_group" "allow_http_inbound" {
+  type              = "ingress"
+  security_group_id = aws_security_group.alb.id
 
-  egress {
-    from_port   = local.any_port
-    to_port     = local.any_port
-    protocol    = local.any_protocol
-    cidr_blocks = local.all_ips
-  }
+  from_port   = local.http_port
+  to_port     = local.http_port
+  protocol    = local.tcp_protocol
+  cidr_blocks = local.all_ips
+}
+
+resource "aws_security_group" "allow_all_outbound" {
+  type              = "egress"
+  security_group_id = aws_security_group.alb.id
+
+  from_port   = local.any_port
+  to_port     = local.any_port
+  protocol    = local.any_protocol
+  cidr_blocks = local.all_ips
 }
