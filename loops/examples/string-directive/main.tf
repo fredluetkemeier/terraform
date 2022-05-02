1f6680d23ed6338a3a33c16f9f056ec7b4164764
Fred Luetkemeier <fredluetkemeier@gmail.com>
2022-05-02 08:09:06 -0500

    String directive example

---
+++ loops/examples/string-directive/main.tf
@@ -0,0 +1,13 @@
+variable "names" {
+  description = "Names to render"
+  type        = list(string)
+  default     = ["neo", "trinity", "morpheus"]
+}
+
+output "for_directive" {
+  value = <<EOF
+    %{for name in var.names}
+        ${name}
+    %{endfor}
+    EOF
+}
